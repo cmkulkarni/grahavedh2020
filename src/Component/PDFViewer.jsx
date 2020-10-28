524fb8659f5d9a7b6d0eea079f447fa5a1674430
Chaitanya Mukund Kulkarni <cmkulk@gmail.com>
2020-10-28 13:41:12 +0530

    add; initial commit

--- src/Component/PDFViewer.jsx
+++ src/Component/PDFViewer.jsx
@@ -1,24 +1,36 @@
 import react from 'react';
 import classes from './mystyle.module.css';
 import pdf from '../doc/2019.pdf';
+import one from '../doc/2019/20191024_1.jpg';
+import two from '../doc/2019/20191024_2.jpg';
+import three from '../doc/2019/20191024_3.jpg';
+import paths from './MagzinPath.json'
 
 const PDFViewer = props => {
     var pdfPath = 'https://github.com/cmkulkarni/grahavedh2020/raw/master/src/doc/2019.pdf';
-    var h = window.innerHeight*0.65;
-    var scrollBool = '&scrollbar=0';
-    //pdfPath = pdfPath + scrollBool;
+    var h = window.innerHeight*0.80;
+    var imgArr = [
+        <img key={1} src={one} alt="Image not loaded" width="90%" ></img>,
+        <img key={2} src={two} alt="Image not loaded" width="90%" height={h}></img>,
+        <img key={3} src={three} alt="Image not loaded" width="90%" height={h}></img>,
+    ];
+
     return(
         <>
-            <div style={{height: h}} className={classes.pdfViewer}>
-                <iframe frameborder="0" scrolling="no"
-                        src={pdfPath} width="90%" height="700px">
-                </iframe>
-                
+            <div style={{height: h}} className={classes.pdfViewer}>                
+                {imgArr}              
             </div>
         </>
     );
 }
+
+export default PDFViewer;
+
 /*<object id="myPdf" type="application/pdf" data={pdfPath} width="90%" height="700px">
                     Unable to open the PDF file.
-                </object>*/
-export default PDFViewer;
+                </object>
+                <iframe frameborder="0" scrolling="no"
+                        src={pdfPath} width="90%" height="700px">
+                </iframe>
+                
+                */
